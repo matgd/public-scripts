5ad35084179e029c72c667468216164fdf1a70c5
matgd <matgd@users.noreply.github.com>
2026-02-16 01:51:44 +0100

    exchange_rate 2nd provider

--- bash/exchange_rate.sh
+++ bash/exchange_rate.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 function help() {
-    echo "Usage: $0 <from_currency> <to_currency> [format]"
+    echo "Usage: $0 <from_currency> <to_currency> <provider> [format] [decimal_places]"
     echo
     echo "Arguments:"
     echo "  from_currency   The currency code to convert from (e.g., USD)"
     echo "  to_currency     The currency code to convert to (e.g., EUR)"
+    echo "  provider        The exchange rate provider to use (e.g., frankfurter, er-api)"
     echo "  format          Optional format string with tokens:"
     echo "                    {rate} - The exchange rate"
-    echo "                    {rate:2f} - The exchange rate formatted to 2 decimal places"
     echo "                    {from} - The source currency code"
     echo "                    {to} - The target currency code"
     echo "                    {date} - The date of the exchange rate"
@@ -17,6 +17,13 @@ function help() {
     echo "                    {emoji_eur} - Emoji for EUR"
     echo "                    {emoji_gbp} - Emoji for GBP"
     echo "                    {emoji_jpy} - Emoji for JPY"
+    echo " decimal_places   Optional number of decimal places to format the exchange rate"
+    echo
+    echo "Environment Variables:"
+    echo "  XDG_CACHE_HOME       Directory for cache files (default: \$HOME/. cache)"
+    echo "  CACHED_FILE          Path to the cached exchange rate file (default: \$XDG_CACHE_HOME/exchange_rate.<provider>.<from_currency>.<to_currency>.json)"
+    echo "  CACHE_TTL_SECONDS    Time-to-live for the cache in seconds (default: 18000, i.e., 5 hours)"
+    echo "  QUIET_CURL           If set to true, suppresses curl output (default: true)"
 }
 
 if [ "$#" -lt 2 ]; then
@@ -26,14 +33,25 @@ fi
 
 FROM_CURRENCY="${1}"
 TO_CURRENCY="${2}"
-FORMAT="${3:-}"
+PROVIDER="${3:-}"
+FORMAT="${4:-}"
+DECIMAL_PLACES="${5:-4}"
+
+if [ "$PROVIDER" = "frankfurter" ]; then
+    URL="https://api.frankfurter.app/latest?from=${FROM_CURRENCY}&to=${TO_CURRENCY}"
+elif [ "$PROVIDER" = "er-api" ]; then
+    URL="https://open.er-api.com/v6/latest/${FROM_CURRENCY}"
+else
+    echo "Unsupported provider: $PROVIDER"
+    help
+    exit 1
+fi
 
 XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-CACHED_FILE="${CACHED_FILE:-$XDG_CACHE_HOME/exchange_rate.$FROM_CURRENCY.$TO_CURRENCY.json}"
+CACHED_FILE="${CACHED_FILE:-$XDG_CACHE_HOME/exchange_rate.$PROVIDER.$FROM_CURRENCY.$TO_CURRENCY.json}"
 CACHE_TTL_SECONDS="${CACHE_TTL_SECONDS:-18000}"  # 5 hours
 QUIET_CURL="${QUIET_CURL:-true}"
 
-URL="https://api.frankfurter.app/latest?from=${FROM_CURRENCY}&to=${TO_CURRENCY}"
 
 QUIET=$QUIET_CURL cached_curl \
   "$URL" \
@@ -46,15 +64,22 @@ if [ -z "$FORMAT" ]; then
   exit 0
 fi
 
+if [ "$PROVIDER" = "frankfurter" ]; then
+    EX_RATE=$(jq -r ".rates[\"$TO_CURRENCY\"]" "$CACHED_FILE")
+    EX_DATE=$(jq -r .date "$CACHED_FILE")
+elif [ "$PROVIDER" = "er-api" ]; then
+    EX_RATE=$(jq -r ".rates[\"$TO_CURRENCY\"]" "$CACHED_FILE")  # same
+    EX_UNIX_DATE=$(jq -r .time_last_update_unix "$CACHED_FILE")
+    EX_DATE=$(date -d "@$EX_UNIX_DATE" +"%Y-%m-%d %H:%M:%S")
+fi
+
 LC_NUMERIC=C
 
-RATE=$(jq -r ".rates[\"$TO_CURRENCY\"]" "$CACHED_FILE")
 declare -A FORMAT_TOKENS=(
-  ["{rate}"]="$RATE"
-  ["{rate:2f}"]="$(printf "%.2f" "$RATE")"
+  ["{rate}"]="$(printf "%.${DECIMAL_PLACES}f" "$EX_RATE")"
   ["{from}"]="$FROM_CURRENCY"
   ["{to}"]="$TO_CURRENCY"
-  ["{date}"]="$(jq -r .date "$CACHED_FILE")"
+  ["{date}"]="$EX_DATE"
   ["{emoji_usd}"]="💵"
   ["{emoji_eur}"]="💶"
   ["{emoji_gbp}"]="💷"
